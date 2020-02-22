18bc0f9ffd08c745304fce4dd53d5756760811bb
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-22 23:22:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song 
+  
+    @@count = 0  
+    @@artists = []
+    @@genre = []
+    
+attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+   end
+
+
+  def self.genre
+    @@genre.uniq
+  end
+  
+  def self.artist
+    @@artist.uniq
+  end
+  
+  def artist_count
+    
+    #push the artist name and count 
+    @@artist
+    
+  end
+  
+  def genre_count
+    # push the genre and number of genre
+    @@genre_count = {}
+    
+  end
+  
+  
+end
+  
+
+
